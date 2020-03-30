7a2ff858364e452eaaf5cb17ac7637470830f942
Sym <lanyajq@outlook.com>
2020-03-30 11:59:22 +0800

    V1001
    真第一个版本

--- Ezfic.cs
+++ Ezfic.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.IO;
 using System.Collections.Generic;
 using Terraria;
@@ -13,23 +13,62 @@ using Terraria.ModLoader;
 using ReLogic.Graphics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System.Windows;
+using System.Windows.Forms;
+using System.Reflection;
 
 namespace Ezfic
 {
     public class Ezfic: Mod
     {
+        public static Mod mod;
+        static Texture2D logo1;
+        static Texture2D logo2;
         public Ezfic()
-
         {
-            Properties = new ModProperties()
+            mod = this;
+        }
+        public override void Load()
+        {
+            logo1 = Main.logoTexture;
+            logo2 = Main.logo2Texture;
+            Main.logoTexture = GetTexture("logo1");
+            Main.logo2Texture = GetTexture("logo2");
+            Main.versionNumber = "Cheateria V1.4";
+            Main.versionNumber2 = "Cheateria";
+            SetLang("LegacyMenu.12", "单人作弊");
+            SetLang("LegacyMenu.13", "多人作弊");
+            SetLang("LegacyInterface.50", "试一下你");
+        }
+        public override void PostUpdateEverything()
+        {
+        }
+        public override void Unload()
+        {
+            base.Unload();
+            Main.logoTexture = logo1;
+            Main.logo2Texture = logo2;
+            Form alphaForm = new Form();
+            alphaForm.Text = "实锤你尝试卸载此mod!不过mod确实已被你卸载。(你或许没有卸载？谁在乎呢)";
+            alphaForm.Size = new System.Drawing.Size(1500, 20);
+            //alphaForm.ShowDialog();
+            //new Main();
+        }
+        public static bool SetLang(string key, string text)
+        {
+            FieldInfo alphaInfo = typeof(LanguageManager).GetField("_localizedTexts", BindingFlags.NonPublic | BindingFlags.Instance);
+            Dictionary<string, LocalizedText> dic = (Dictionary<string, LocalizedText>)alphaInfo.GetValue(LanguageManager.Instance);
+            LocalizedText alphaText;
+            if (dic.TryGetValue(key, out alphaText))
             {
-
-                Autoload = true,
-                AutoloadGores = true,
-                AutoloadSounds = true,
-                AutoloadBackgrounds = true
-
-            };
+                MethodInfo betaInfo = alphaText.GetType().GetMethod("SetValue", BindingFlags.NonPublic | BindingFlags.Instance);
+                object[] alphaObj = { text };
+                betaInfo.Invoke(alphaText, alphaObj);
+                dic.Remove(key);
+                dic.Add(key, alphaText);
+                return true;
+            }
+            return false;
         }
     }
 }
---
+++ EzficItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Ezfic
+{
+    class EzficItem : GlobalItem
+    {
+        public override void SetDefaults(Item item)
+        {
+        }
+        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+        {
+            if(item.type == ItemID.StarCannon)
+            {
+                tooltips.Add(new TooltipLine(Ezfic.mod, "FishingPower", "顶级玩家的选择"));
+            }
+        }
+    }
+}
